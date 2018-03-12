a1c148fe03fd0976bbc54a58b52b57f4e592960e
Victor Chaplin <victorchaplin@outlook.com>
2018-03-11 22:36:04 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const mongoose = require('./db/mongoose')
 const Todo = require('./models/todo')
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
         res.send({todos})
     }, (error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send()
+        }
+
+        res.send({todo})
+    }).catch((error) => {
+        res.status(400).send()
     })
 })
 
