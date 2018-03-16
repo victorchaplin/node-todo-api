0274628521f99089845f59dae1c807a7bedf3027
Victor Chaplin <victorchaplin@outlook.com>
2018-03-16 01:38:02 -0300

    adding first DELETE test

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send()
         }
-        res.status(200).send({todo})
+        res.send({todo})
         }).catch((todo) => {
             res.status(400).send()
     })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,34 @@ describe('GET /todos/:id', () => {
             .end(done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        const hexId = todos[1]._id.toHexString()
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId)
+            })
+            .end((error, res) => {
+                if (error) {
+                    return done(error)
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBeNull()
+                    done()
+                }).catch((error) => done(error))
+            })
+    })
+
+    // it('should return 404 if todo not found', (done) => {
+
+    // })
+
+    // it('should return 404 if object id is invalid', (done) => {
+
+    // })
+})
