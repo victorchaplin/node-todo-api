0faaa8e1b773fecffd4a2e3fc24b298cb007f7ea
Victor Rocha <victor.rocha@zenvia.com>
2018-05-21 11:38:37 -0300

    starting login implementation

--- server/server.js
+++ server/server.js
@@ -111,6 +111,16 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user)
 })
 
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        res.send(user)
+    }).catch(error => {
+        res.status(400).send()
+    })
+})
+
 app.listen(port, () => {
     console.log(`Started server on port ${port}`)
 })
